768d9d2ec6d36b4b0e1f884595bf3ec8fcec3fde
Jiahui.wen <jiahuiwen622111@gmail.com>
2013-08-27 15:39:06 +0800

    create project

---
+++ ExpressSearcher/src/main/java/org/wen/express/AppContext.java
@@ -0,0 +1,14 @@
+package org.wen.express;
+
+import android.app.Application;
+
+/**
+ * Created by wenjiahui on 8/27/13.
+ */
+public class AppContext extends Application{
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
